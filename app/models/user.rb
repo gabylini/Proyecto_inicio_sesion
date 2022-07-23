587045786ba3a19a8f96df0615ad0b35e3906327
gabylini <mgabriela.rc@gmail.com>
2022-07-23 13:33:29 -0400

    "Actividad final"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true, length: {minimum:2, too_short: ":Nombre debe tener minimo 2 caracteres"}
+    validates :last_name, presence: true, length: {minimum:2, too_short: ":Apellido debe tener minimo 2 caracteres"}
+    validates :email_address, presence: true 
+    validates :age, presence: true, numericality:{greater_than:10, less_than:150, message: ":Edad minima 10 y maxima 150"}
 end
